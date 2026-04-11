8c0f2b9684fb31acff247f77fc67547642a73709
groofazy <gabe.gamboa05@gmail.com>
2026-04-10 21:00:46 -0700

    added routes for edit and insert pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import AthletesPage from './pages/AthletesPage'
+import AthleteEditPage from './pages/EditPage'
+import AthleteInsertPage from './pages/InsertPage'
+
+
 
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<AthletesPage />} />
+      <Route path="/" element={<AthletesPage/>} />
       <Route path="/athletes" element={<AthletesPage />} />
+      <Route path="/athletes/:id/edit" element={<AthleteEditPage />} />
+      <Route path="/athletes/new" element={<AthleteInsertPage />} />
     </Routes>
     </BrowserRouter>
   )
