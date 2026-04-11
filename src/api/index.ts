67ee8a06cdd51af365bf0f56df50ddd0c8f2a603
groofazy <gabe.gamboa05@gmail.com>
2026-04-10 21:03:30 -0700

    added AthleteFormData used for Insert and Edit pages

--- src/api/index.ts
+++ src/api/index.ts
@@ -13,3 +13,36 @@ export const deleteAthlete = async (id: number): Promise<{ message: string }> =>
   })
   return res.json()
 }
+
+/*
+  insert all other API calls underneath
+*/
+
+export const addAthlete = async (athlete: {
+  id: number
+  name: string
+  email: string
+  highSchool: string
+  position: string
+}): Promise<{ message: string }> => {
+  const res = await fetch(`${BASE_URL}/athletes`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(athlete)
+  })
+  return res.json()
+}
+
+export const editAthlete = async (id: number, athlete: {
+  name: string
+  email: string
+  highSchool: string
+  position: string
+}): Promise<{ message: string }> => {
+  const res = await fetch(`${BASE_URL}/athletes/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(athlete)
+  })
+  return res.json()
+}
